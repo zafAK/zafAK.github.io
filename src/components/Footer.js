15b8e12932cdc2c68e644b2831a6e4c2e537270c
Zafar <zz45239173@gmail.com>
2025-02-23 12:26:19 -0600

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
           <h3>Designed and Developed by Zafar Khan</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} ZK</h3>
+          <h3>Copyright {year} ZK</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
